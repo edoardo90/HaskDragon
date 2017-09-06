76cf1c3fda52e7c4f0673519730819b325736d47
Edoardo <edoardo.gacc@gmail.com>
2017-09-06 19:59:12 +0200

    refactoring

--- Handlers/Player/PlayerHandler.hs
+++ Handlers/Player/PlayerHandler.hs
@@ -20,7 +20,6 @@ import Model.GameMap (Map(..), Board(..), Item(..), boardToBBound)
 import Model.PlayerInfo (Player(..), newPlayer, addItemToPlayer)
 
 -- tools
-import Data.Maybe (fromMaybe, fromJust, isJust, isNothing)
 import qualified Data.ByteString.Lazy.Char8  as L8
 import qualified Data.ByteString.Char8  as BS8
 import qualified Data.ByteString as BS
@@ -33,15 +32,15 @@ postGamePlayerHomeR :: Yesod master => PlayerName -> HandlerT GamePlayer (Handle
 postGamePlayerHomeR playerName = lift $ readTeamAndJson ( `createAndSavePlayer` playerName )
 
 createAndSavePlayer :: Yesod master => BS.ByteString -> PlayerName ->  (HandlerT master IO) Value
-createAndSavePlayer teamHeader playerName =
-  do
+createAndSavePlayer teamHeader playerName = do
     let player' = newPlayer (T.unpack playerName)
     savedPlayer <- lift $ Red.saveJsonPlayer teamHeader (TE.encodeUtf8 playerName) player'
-    if isJust savedPlayer then
-      sendResponseStatus status201  $ toJSON player'
-    else do
-      let playerStr = T.unpack playerName
-      sendResponseStatus status409  $ object ["err" .= ("player " ++  playerStr ++  " is already present " :: String)]
+    responseForSavedPlayer savedPlayer playerName
+
+responseForSavedPlayer (Just savedPlayer') _ =  sendResponseStatus status201  $ toJSON savedPlayer'
+responseForSavedPlayer Nothing playerName =
+   sendResponseStatus status409  $ object ["err" .= ("player " ++  T.unpack playerName ++  " is already present " :: String)]
+
 
 
 postGamePlayerCollectR :: Yesod master => PlayerName -> SphereId -> HandlerT GamePlayer (HandlerT master IO) Value
@@ -49,32 +48,40 @@ postGamePlayerCollectR playerName sphereId = lift $ readTeamAndJson (collectSphe
 
 collectSphere' ::  Yesod master => SphereId -> PlayerName -> BS.ByteString  -> (HandlerT master IO) Value
 collectSphere' sphereId playerName team = do
-  m <- lift $ Red.getMapJsonById team
-  p <- getGamePlayer' playerName team
-  if isNothing m then
-    return $ object ["msg" .= (" no map found for specified team" :: String)]
-  else
-    if isNothing p then
-      return $ object ["msg" .= ("no player with such name" :: String)]
-    else do
+  map' <- lift $ Red.getMapJsonById team
+  player' <- lift $ Red.getPlayerById team (TE.encodeUtf8 playerName)
+  handleCollecting map' player' team sphereId
 
-      let (Board (Map gameSpheres) time) = fromJust m
-      let player@(Player name playerSpheres) = fromJust p
-      let foundSpheres = filter (\s -> Model.GameMap.id s == sphereId) gameSpheres
-      let playerWithNewSphere =  foldr addItemToPlayer player foundSpheres
+handleCollecting :: MonadIO m => Maybe Board -> Maybe Player -> BS.ByteString -> SphereId -> m Value
+handleCollecting Nothing _ _ _ = return $ object ["msg" .= (" no map found for specified team" :: String)]
+handleCollecting _ Nothing _ _ = return $ object ["msg" .= ("no player with such name" :: String)]
+handleCollecting (Just map') (Just player') team sphereId  = do
+    let (name, playerWithNewSphere, gameSpheres) = getNamePlayerSpheres map' player' sphereId
+    savedPlayer <- liftIO $ Red.updateJsonPlayer team (BS8.pack name) playerWithNewSphere
+    return $ msgForCollectedSphere savedPlayer gameSpheres
 
-      savedPlayer <- liftIO $ Red.updateJsonPlayer team (BS8.pack name) playerWithNewSphere
 
-      if isNothing savedPlayer then return $ object ["msg" .= ("player was not correctly saved" :: String)] else do
+getNamePlayerSpheres :: Board -> Player -> Int -> (String, Player, [Item])
+getNamePlayerSpheres map' player' sphereId =
+  let (Board (Map gameSpheres) time) = map'
+      player@(Player name playerSpheres) = player'
+      foundSpheres = filter (\s -> Model.GameMap.id s == sphereId) gameSpheres
+      playerWithNewSphere =  foldr addItemToPlayer player foundSpheres
+  in  (name, playerWithNewSphere, gameSpheres)
 
-        let toGo = length gameSpheres - (length . _spheres . fromJust)  savedPlayer
-        let msg = if toGo == 0 then "Good, you found all of them" else "Still " ++ show toGo ++ " to go!"
 
-        return $ object ["status" .= msg
-                         , "missingSpheres" .= toGo
-                         , "spheres" .=   _spheres (fromJust savedPlayer)
-                         , "playerNow" .= fromMaybe (object ["msg:" .= ("hmm?" :: String)]) (toJSON <$> savedPlayer)
-                         ]
+msgForCollectedSphere :: Foldable t => Maybe Player -> t a -> Value
+msgForCollectedSphere Nothing _ = object ["msg" .= ("player was not correctly saved" :: String)]
+msgForCollectedSphere (Just player) gameSpheres =
+  let toGo = length gameSpheres - (length . _spheres) player
+      msg = if toGo == 0 then "Good, you found all of them" else "Still " ++ show toGo ++ " to go!"
+  in
+    object ["status" .= msg
+                     , "missingSpheres" .= toGo
+                     , "spheres" .=   _spheres player
+                     , "playerNow" .= toJSON player
+                     ]
+
 
 getGamePlayerInfoR :: Yesod master => PlayerName -> HandlerT GamePlayer (HandlerT master IO) Value
 getGamePlayerInfoR playerName = lift $ readTeamAndJson $
@@ -83,9 +90,6 @@ getGamePlayerInfoR playerName = lift $ readTeamAndJson $
               maybe (sendResponseStatus status404  messagePlayerNotFuond) (return $ return $ toJSON p) p
 
 messagePlayerNotFuond = object ["msg" .= ("player not found" :: String)]
-
-getGamePlayer' ::  Yesod master => PlayerName -> BS.ByteString ->  (HandlerT master IO) (Maybe Player)
-getGamePlayer' playerName team = lift $ Red.getPlayerById team (TE.encodeUtf8 playerName)
 
 
 readTeamOrFallback :: MonadHandler m => (BS.ByteString -> m Value) -> m Value -> m Value
--- automation/deploy/ambass.js
+++ automation/deploy/ambass.js
@@ -27,31 +27,33 @@ function build() {
       console.log("4 - BUILD  - ls *.yaml: \n".bold)
       shell.ls('*.yaml').forEach(file => console.log( "4 - D ", file))
 
-      shell.exec(stackBuildCmd, function(code, stdout, stderr) {
-        console.log('5 - after build command - Exit code:'.yellow, code);
-
-        if(code) {
-          console.log("5A - build failed");
-          console.log('5A - Program stderr:', stderr.slice(0, 200), "[..]");
-          return correctBuild(stderr).then(res => resolve(res));
-
-        } else {
-          console.log("build success".green);
-
-          let install = (stderr.match(/Installing executable\(s\).+\s+\/project\/(.+)/i))
-          if(install && (install = install[1])){
-            console.log("install dir :", install)
-            return resolve(install);
-          }
-
-          console.log('6 - Program output:', stdout);
-          console.log(" 6 - ::::::::::::. after build command".yellow)
-          shell.ls('*.yaml').forEach(file => console.log(" 6 - AB - ", file))
-
-          return resolve('');
-        }
-      });
+      shell.exec(stackBuildCmd, afterStackBuildCmd);
     })
+}
+
+function afterStackBuildCmd(code, stdout, stderr) {
+  console.log('5 - after build command - Exit code:'.yellow, code);
+
+  if(code) {
+    console.log("5A - build failed");
+    console.log('5A - Program stderr:', stderr.slice(0, 200), "[..]");
+    return correctBuild(stderr);
+
+  } else {
+    console.log("build success".green);
+
+    let install = (stderr.match(/Installing executable\(s\).+\s+\/project\/(.+)/i))
+    if(install && (install = install[1])){
+      console.log("install dir :", install)
+      return resolve(install);
+    }
+
+    console.log('6 - Program output:', stdout);
+    console.log(" 6 - ::::::::::::. after build command".yellow)
+    shell.ls('*.yaml').forEach(file => console.log(" 6 - AB - ", file))
+
+    return resolve('');
+  }
 }
 
 function correctBuild(err){
@@ -66,47 +68,49 @@ function correctBuild(err){
       console.log("7A - SOLVER ERROR - stack solver way ")
       console.log(" stack solver --system-ghc --update-config ".white)
 
-      shell.exec('stack solver --system-ghc --update-config', function(code, solverOut, solverErr){
+      shell.exec('stack solver --system-ghc --update-config',
+          (code, solverOut, solverErr) => afterStackSolver(code, solverOut, solverErr, err));
 
-        if(code){
-          console.log("7.1A - stack solver was not successfull  ".red)
-          console.log("solver err:", solverErr)
-          return resolve('');
-        }
-        else {
-          console.log("7.1B - stack solver was successfull .. ".yellow)
-
-          let unchanged = solverErr.match(/No changes needed/gi);
-
-          if(unchanged){
-            console.log(" 7.2A - .. no changes needed".red);
-            if(err.match(/try adding the following to your extra-deps in/gi)){
-              let m = err.match(/try adding the following to your extra-deps in \/project\/stack\.yaml:\s+/)
-              var ms = (err.match(/try adding the following to your extra-deps in.+\s+(.+\s+)+\s+You may also/i))
-
-              if(ms && ms[1]){
-                console.log("deps:", ms[1])
-                return resolve(ms[1]);
-              }
-
-            } else {
-              return build().then(res => resolve(res));
-            }
-
-          } else {
-            console.log(" 7.2B - .. build configuration corrected, lets try!".green);
-            console.log("stack solver out:".yellow, solverOut.slice(0, 100), "[...]".green,  solverOut.slice(-100) )
-            console.log(" 7.3 - build corrected!".green);
-            return  build().then(res => resolve(res));
-          }
-        }
-      })
     } else {
       console.log(" 7B - NO SOLVER ERROR :".red, err, "7B - message:", err, " we don't know how to fix it")
-      return build().then(res => resolve(res));
+      return build();
     }
 
   })
+}
+
+function afterStackSolver(code, solverOut, solverErr, err){
+
+  if(code){
+    console.log("7.1A - stack solver was not successfull  ".red)
+    console.log("solver err:", solverErr)
+    return resolve('');
+  }
+  else {
+    console.log("7.1B - stack solver was successfull .. ".yellow)
+
+    let unchanged = solverErr.match(/No changes needed/gi);
+
+    if(unchanged){
+      console.log(" 7.2A - .. no changes needed".red);
+      if(err.match(/try adding the following to your extra-deps in/gi)){
+        let m = err.match(/try adding the following to your extra-deps in \/project\/stack\.yaml:\s+/)
+        var ms = (err.match(/try adding the following to your extra-deps in.+\s+(.+\s+)+\s+You may also/i))
+
+        if(ms && ms[1]){
+          console.log("deps:", ms[1])
+          return resolve(ms[1]);
+        }
+      } else {
+        return build();
+      }
+    } else {
+      console.log(" 7.2B - .. build configuration corrected, lets try!".green);
+      console.log("stack solver out:".yellow, solverOut.slice(0, 100), "[...]".green,  solverOut.slice(-100) )
+      console.log(" 7.3 - build corrected!".green);
+      return  build();
+    }
+  }
 }
 
 
@@ -124,17 +128,12 @@ function postBuild(instsallDir){
   });
 }
 
-function sshUploadBin(installDir) {
-
-
-
-}
 
 
 preBuild()
-  // .then(build)
-  // .then(postBuild)
-  .then(sshUploadBin);
+   .then(build)
+   .then(postBuild)
+
 
 
 //
